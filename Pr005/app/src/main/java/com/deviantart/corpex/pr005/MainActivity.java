a74004c0a85e73565b4876510b626cc4865cc6ff
corpex <corpex@gmx.com>
2015-10-03 00:18:23 +0200

    Pr005
    Añadido funcionalidad toast y activacion/desactivacion del boton aceptar

--- Pr005/app/src/main/java/com/deviantart/corpex/pr005/MainActivity.java
+++ Pr005/app/src/main/java/com/deviantart/corpex/pr005/MainActivity.java
@@ -2,36 +2,91 @@ package com.deviantart.corpex.pr005;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etUsuario;
+    EditText etContraseña;
+    Button btnAceptar;
+    Button btnCancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void initViews() {
+        etUsuario = (EditText) findViewById(R.id.etUsuario);
+        etContraseña = (EditText) findViewById(R.id.etContraseña);
+        btnAceptar = (Button) findViewById(R.id.btnAceptar);
+        btnAceptar.setOnClickListener(this);
+        btnCancelar = (Button) findViewById(R.id.btnCancelar);
+        btnCancelar.setOnClickListener(this);
+
+        etUsuario.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                tieneDatos();
+            }
+        });
+
+        etContraseña.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                tieneDatos();
+            }
+        });
+
+
+        tieneDatos();
     }
 
+    private void tieneDatos() {
+        if(!TextUtils.isEmpty(etUsuario.getText()) && !TextUtils.isEmpty(etContraseña.getText()))
+            btnAceptar.setEnabled(true);
+        else
+            btnAceptar.setEnabled(false);
+    }
+
+
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onClick(View v) {
+       switch(v.getId()){
+           case R.id.btnAceptar:
+               Toast.makeText(MainActivity.this, "Estableciendo conexion con usuario "+etUsuario.getText(), Toast.LENGTH_LONG).show();
+               break;
+           case R.id.btnCancelar:
+               break;
+       }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
