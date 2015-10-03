cdf891411507fc42e4e727f0345115ab523b812b
corpex <corpex@gmx.com>
2015-10-03 17:38:40 +0200

    Añadido cambios visuales.

--- Pr005/app/src/main/java/com/deviantart/corpex/pr005/MainActivity.java
+++ Pr005/app/src/main/java/com/deviantart/corpex/pr005/MainActivity.java
@@ -1,5 +1,7 @@
 package com.deviantart.corpex.pr005;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,7 +10,9 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     EditText etContraseña;
     Button btnAceptar;
     Button btnCancelar;
+    TextView tvUsuario;
+    TextView tvContraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnAceptar.setOnClickListener(this);
         btnCancelar = (Button) findViewById(R.id.btnCancelar);
         btnCancelar.setOnClickListener(this);
+        tvUsuario = (TextView) findViewById(R.id.tvUsuario);
+        tvContraseña = (TextView) findViewById(R.id.tvContraseña);
+
+        etUsuario.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                cambiarColor(tvUsuario, hasFocus);
+            }
+        });
+
+        etContraseña.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                cambiarColor(tvContraseña, hasFocus);
+            }
+        });
 
         etUsuario.addTextChangedListener(new TextWatcher() {
             @Override
@@ -46,6 +68,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void afterTextChanged(Editable s) {
                 tieneDatos();
+                checkVisibility(etUsuario, tvUsuario);
             }
         });
 
@@ -63,11 +86,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void afterTextChanged(Editable s) {
                 tieneDatos();
+                checkVisibility(etContraseña, tvContraseña);
             }
         });
 
 
         tieneDatos();
+        checkVisibility(etContraseña, tvContraseña);
+        checkVisibility(etUsuario, tvUsuario);
+    }
+
+    private void cambiarColor(TextView tv, boolean hasFocus) {
+        if(hasFocus){
+            tv.setTextColor(Color.GREEN);
+        }
+        else{
+            tv.setTextColor(Color.LTGRAY);
+        }
     }
 
     private void tieneDatos() {
@@ -85,8 +120,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                Toast.makeText(MainActivity.this, "Estableciendo conexion con usuario "+etUsuario.getText(), Toast.LENGTH_LONG).show();
                break;
            case R.id.btnCancelar:
+               //tvUsuario.setText("");
+               //tvContraseña.setText("");
+               etUsuario.setText("");
+               etContraseña.setText("");
+              // etUsuario.setHint("Usuario");
+             //  etContraseña.setHint("Contraseña");
                break;
        }
 
+    }
+
+    private void checkVisibility(EditText txt, TextView lbl) {
+        if (TextUtils.isEmpty(txt.getText().toString())) {
+            lbl.setVisibility(View.INVISIBLE);
+        } else {
+            lbl.setVisibility(View.VISIBLE);
+        }
     }
 }
